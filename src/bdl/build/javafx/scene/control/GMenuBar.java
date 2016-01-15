70456c62c8f231fc0bfd90e77de8e5593c590ecc
Georg <Georg@GeorgW10DT>
2016-01-15 19:01:07 +0100

    Menubuilder Erstintegration

--- src/bdl/build/javafx/scene/control/GMenuBar.java
+++ src/bdl/build/javafx/scene/control/GMenuBar.java
@@ -1,56 +1,83 @@
 package bdl.build.javafx.scene.control;
 
-import bdl.build.GObject;
-import bdl.view.right.PropertyEditPane;
-import bdl.build.properties.PanelProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.scene.control.MenuBar;
-
 import java.util.List;
 
+import bdl.build.GObject;
+import bdl.build.properties.PanelProperty;
+import bdl.view.right.PropertyEditPane;
+import di.menubuilder.MenuBuilder;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.MenuBar;
+
 public class GMenuBar extends MenuBar implements GObject {
-    private String fieldName;
-    private List<PanelProperty> properties;
-    private PropertyEditPane pep;
-    private StringProperty fieldNameProperty = new SimpleStringProperty();
+	private List<PanelProperty> properties;
+	private PropertyEditPane pep;
+	private StringProperty fieldNameProperty = new SimpleStringProperty();
+	private MenuBuilder menuBuilder;
 
-    @Override
-    public String getFieldName() {
-        return fieldNameProperty.getValue();
-    }
-
-    @Override
-    public void setFieldName(String fieldName) {
-        fieldNameProperty.setValue(fieldName);
-    }
-
-    @Override
-    public StringProperty fieldNameProperty() {
-        return fieldNameProperty;
-    }
-
-    @Override
-    public void setPanelProperties(List<PanelProperty> properties) {
-        this.properties = properties;
+	public GMenuBar(){
+    	super();
+    	menuBuilder= new MenuBuilder(this);
+    	fieldNameProperty.addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {	
+				if (menuBuilder.getFieldName()!=null&&menuBuilder.getFieldName().equals(newValue)){
+					return;
+				}
+				menuBuilder.setFieldName(newValue);				
+			}
+		});
     }
 
-    @Override
-    public List<PanelProperty> getPanelProperties() {
-        return properties;
-    }
+	public MenuBuilder getMenuBuilder() {
+		return menuBuilder;
+	}
 
-    @Override
-    public void setPEP(PropertyEditPane propertyEditPane) {
-        pep = propertyEditPane;
-    }
-    
-    @Override
-    public PropertyEditPane getPEP() {
-        return pep;
-    }
-    @Override
-   	public String getNodeClassName() {		
-   		return "MenuBar";
-   	}
+	
+
+	@Override
+	public String getFieldName() {
+		return fieldNameProperty.getValue();
+	}
+
+	@Override
+	public void setFieldName(String fieldName) {
+		if (getFieldName()!=null && getFieldName().equals(fieldName)) {
+			return;
+		}
+		fieldNameProperty.setValue(fieldName);
+	}
+
+	@Override
+	public StringProperty fieldNameProperty() {
+		return fieldNameProperty;
+	}
+
+	@Override
+	public void setPanelProperties(List<PanelProperty> properties) {
+		this.properties = properties;
+	}
+
+	@Override
+	public List<PanelProperty> getPanelProperties() {
+		return properties;
+	}
+
+	@Override
+	public void setPEP(PropertyEditPane propertyEditPane) {
+		pep = propertyEditPane;
+	}
+
+	@Override
+	public PropertyEditPane getPEP() {
+		return pep;
+	}
+
+	@Override
+	public String getNodeClassName() {
+		return "MenuBar";
+	}
 }
